1d3bda3ec6e23061f7d5b3d3ff5708460f84e73f
Daniel Almeida <dasn@ecomp.poli.br>
2024-02-25 14:25:53 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+// Nesse caso não precisa da Anotation @Repository, porque já herda de JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service // serve para registrar como um serviço no spring
+public class UserService {
+
+	@Autowired // faz a injeção de dependência de maneira automática entre UserService e
+				// UserRepo
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
