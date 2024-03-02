e26f97e31d5870ba09fe4b735abe05d4f02f1a4b
Daniel Almeida <dasn@ecomp.poli.br>
2024-03-02 12:33:39 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.Product;
+
+// Nesse caso não precisa da Anotation @Repository, porque já herda de JpaRepository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+	
+}
---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+@Service // serve para registrar como um serviço no spring
+public class ProductService {
+
+	@Autowired // faz a injeção de dependência de maneira automática entre ProductService e
+				// ProductRepo
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
